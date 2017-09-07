481f1c97c2d983fa89c7c1cf38f8642630d183ed
Chancer <Chancergs@hotmail.com>
2017-09-07 02:20:11 -0600

    Removed quiz plugin that i accidentally uploaded

--- respite/plugins/quiz/sh_lang.lua
+++
@@ -1,3 +0,0 @@
-nut.lang.Add("quiz_title", "issues")
-nut.lang.Add("quiz_notification", "If the answers to the questions are incorrect, you will be disconnected from the server.")
-nut.lang.Add("quiz_submit", "Submit Answers")
